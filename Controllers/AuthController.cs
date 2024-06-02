89f0124e9dce0b25566e9cef31e2c996521250ae
RaghadMacq <r.naser@macquires.com>
2024-06-02 09:37:14 +0300

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿//using Microsoft.AspNetCore.Authentication;
+//using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
+
+namespace azureAD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenAcquisition _tokenAcquisition;
+
+        public AuthController(ITokenAcquisition tokenAcquisition)
+        {
+            _tokenAcquisition = tokenAcquisition;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest model)
+        {
+            if (AuthAgainstAD(model))
+            {
+                string[] scopes = new string[] { "api://5458ceee-6ee8-4951-923e-7b91c5062a70/.default" };
+                string token = await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+                return Ok(new LoginResponse { Success = true, Token = token });
+            }
+
+            return Unauthorized(new LoginResponse { Success = false, Errors = new[] { "INCORRECT_USERNAME_OR_PASSWORD" } });
+        }
+
+        private bool AuthAgainstAD(LoginRequest model)
+        {
+            bool isValid = false;
+
+            try
+            {
+                using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "NETORGFT6811883.onmicrosoft.com"))
+                {
+                    isValid = pc.ValidateCredentials(model.UserName, model.Password);
+                }
+            }
+            catch (Exception e)
+            {
+                // Log exception
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string Token { get; set; }
+        public string[] Errors { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure Azure AD authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddInMemoryTokenCaches();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserPolicy", policyBuilder =>
+        policyBuilder.RequireClaim("http://schemas.microsoft.com/identity/claims/scope", "access_as_user"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
